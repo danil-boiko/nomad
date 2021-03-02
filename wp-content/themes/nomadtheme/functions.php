5a908499671d0cc418b46c5d54ed3309621b8310
danil-boiko <boiko.81@list.ru>
2021-03-02 16:14:55 +0300

    register new post type

--- wp-content/themes/nomadtheme/functions.php
+++ wp-content/themes/nomadtheme/functions.php
@@ -178,3 +178,45 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
+add_action( 'init', 'register_post_types' );
+function register_post_types(){
+	register_post_type( 'trip', [
+		'label'  => null,
+		'labels' => [
+			'name'               => 'Путешествия', // основное название для типа записи
+			'singular_name'      => 'Путешествие', // название для одной записи этого типа
+			'add_new'            => 'Добавить Путешествие', // для добавления новой записи
+			'add_new_item'       => 'Добавление Путешествия', // заголовка у вновь создаваемой записи в админ-панели.
+			'edit_item'          => 'Редактирование Путешествия', // для редактирования типа записи
+			'new_item'           => 'Новое Путешествие', // текст новой записи
+			'view_item'          => 'Смотреть Путешествия', // для просмотра записи этого типа.
+			'search_items'       => 'Искать Путешествия', // для поиска по этим типам записи
+			'not_found'          => 'Не найдено', // если в результате поиска ничего не было найдено
+			'not_found_in_trash' => 'Не найдено в корзине', // если не было найдено в корзине
+			'parent_item_colon'  => '', // для родителей (у древовидных типов)
+			'menu_name'          => 'Путешествия', // название меню
+		],
+		'description'         => '',
+		'public'              => true,
+		// 'publicly_queryable'  => null, // зависит от public
+		// 'exclude_from_search' => null, // зависит от public
+		// 'show_ui'             => null, // зависит от public
+		// 'show_in_nav_menus'   => null, // зависит от public
+		'show_in_menu'        => null, // показывать ли в меню адмнки
+		// 'show_in_admin_bar'   => null, // зависит от show_in_menu
+		'show_in_rest'        => null, // добавить в REST API. C WP 4.7
+		'rest_base'           => null, // $post_type. C WP 4.7
+		'menu_position'       => 4,
+		'menu_icon'           => null,
+		//'capability_type'   => 'post',
+		//'capabilities'      => 'post', // массив дополнительных прав для этого типа записи
+		//'map_meta_cap'      => null, // Ставим true чтобы включить дефолтный обработчик специальных прав
+		'hierarchical'        => false,
+		'supports'            => [ 'title', 'editor','author','thumbnail','excerpt' ], // 'title','editor','author','thumbnail','excerpt','trackbacks','custom-fields','comments','revisions','page-attributes','post-formats'
+		'taxonomies'          => [],
+		'has_archive'         => true,
+		'rewrite'             => true,
+		'query_var'           => true,
+	] );
+}
+
